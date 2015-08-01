20ee19ff70f7923cd043317ed3458122a0a9b0b8
demesheo <d.wilsonii91@gmail.com>
2015-07-31 17:14:56 -0700

    Get links functional

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,20 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  // console.log(Links);
+
+  $scope.getLinks = function(){
+
+  	// $scope.data.links = data;
+  	Links.getLinks().then(function(data){
+      $scope.data.links = data;
+    });
+
+  };
+
+  $scope.init = function(){
+  	$scope.getLinks();
+  }
+  $scope.init();
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,20 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getLinks = function(){
+  return $http({
+    method : 'GET',
+    url: '/api/links'
+  })
+    .then(function(resp){
+      console.log('**********THIS IS OUR RESPONSE***************', resp)
+      return resp.data;
+    })
+  };
+
+  return {
+    getLinks : getLinks
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
